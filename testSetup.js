a2905c8e43df07c0b17c5875fee16fb9641081f6
jrpillai <jayanrpillai@gmail.com>
2024-04-30 19:08:17 -0500

    add tests for create event and get event

--- testSetup.js
+++ testSetup.js
@@ -1,15 +1,22 @@
 // Using ES6 import statements
+// This file is used to setup the test environment
+
+// Import the dotenv module
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Import the mongoose module
 import mongoose from 'mongoose';
+// Import the MongoMemoryServer module
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
 // Override the MONGO_URI
 process.env.MONGO_URI = process.env.TEST_MONGO_URI;
 
+// Create a variable to store the MongoMemoryServer instance
 let mongoServer;
 
+// Create a beforeAll hook to create the MongoMemoryServer instance
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   process.env.MONGO_URI = mongoServer.getUri();
