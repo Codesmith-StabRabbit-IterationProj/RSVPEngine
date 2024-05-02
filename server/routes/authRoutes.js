f450f0c6be4f7fff0791820b16d1c6d147cdcfc1
Jay Cho <jaycho1017@gmail.com>
2024-05-02 12:05:50 -0500

    login/signup page setup and testing

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,18 @@
 // server/routes/authRoutes.js
-import express from 'express'
-const router = express.Router()
-import userController from '../controller/userController.js'
+import express from 'express';
+const router = express.Router();
+import userController from '../controller/userController.js';
 
 // Route for user logi
 router.post('/login', userController.findUserByUsernameAndPassword, (req, res) => {
-  res.status(200).json(res.locals.user)
-})
+  res.status(200).json(res.locals.user);
+});
 
 // Route for user signup
 router.post('/signup', userController.createUser, (req, res) => {
   // Respond with status 200 and JSON data of the newly created user
-  res.status(200).json(res.locals.newUser)
-})
+  console.log('inside router');
+  res.status(200).json(res.locals.signup);
+});
 
-export default router
+export default router;
